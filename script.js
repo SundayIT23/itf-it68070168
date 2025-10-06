02e1358a9a8233c44a1567efce11230a969fec13
SundayIT23 <68070168@kmitl.ac.th>
2025-10-06 20:55:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const myinput = document.getElementById("myinput")
+const text_area = document.getElementById("text_area");
+const currentAcount = document.getElementById("currentAccount")
+const currentCash = document.getElementById("currentCash")
+let line = 0
+let totalAmount = 0
+let totalCash = 0
 
-function add() {
-    myinput.value = Number(myinput.value) + 1
+function Change() {
+    total_acc = parseFloat(currentAcount.value)
+    total_cash = parseFloat(currentCash.value)
+    line += 1
+    text_area.value += `${line}, Current account balance: ${total_acc}, Current cash balance: ${total_cash}\n`
+}
+
+const option = document.getElementById("select")
+const money = document.getElementById("Deposit/Withdraw")
+
+function Proceed() {
+    if (option.value == "Deposit" && total_cash - money.value >= 0){
+        total_acc += parseFloat(money.value)
+        total_cash -= parseFloat(money.value)
+        line += 1
+        text_area.value += `${line}, Deposit : ${money.value} Current account balance: ${total_acc}, Current cash balance: ${total_cash}\n`
+    }else if (option.value == "Withdraw" && total_acc - money.value >= 0){
+        total_acc -= parseFloat(money.value)
+        total_cash += parseFloat(money.value)
+        line += 1
+        text_area.value += `${line}, Withdraw : ${money.value}, Curren account balance: ${total_acc}, Current cash balance: ${total_cash}\n`
+    }else{
+        text_area.value += `ERROR \n`
+    }
 }
